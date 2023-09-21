b8f9fcdeb382549fdda2e62631eeffd1754b0e7a
CodingManoj <mvp2612@hotmail.com>
2023-09-20 22:34:55 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 module "vpc" {
-  source        = "./vendor/modules/vpc"
-  VPC_CIDR      = var.VPC_CIDR
-  ENV           = var.ENV
+  source              = "./vendor/modules/vpc"
+  VPC_CIDR            = var.VPC_CIDR
+  ENV                 = var.ENV
+  AZ                  = var.AZ 
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
 
 # We cannot parametrize anything that's added n the source
